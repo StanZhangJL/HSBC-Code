ed0a368df702025a2c75a33d47b95775476214bc
Stan Zhang <kuaibu2017@163.com>
2025-08-10 21:39:17 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,31 @@
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+    // arr represents the given list of numbers.
+    public static int[] interchange(int[] arr) {
+        int[] answer = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            answer[arr[i]] = i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // input for arr
+        int arr_size = in.nextInt();
+        int[] arr = new int[arr_size];
+        for (int idx = 0; idx < arr_size; idx++) {
+            arr[idx] = in.nextInt();
+        }
+
+        int[] result = interchange(arr);
+        for (int idx = 0; idx < result.length - 1; idx++) {
+            System.out.print(result[idx] + " ");
+        }
+        System.out.print(result[result.length - 1]);
+    }
+}
